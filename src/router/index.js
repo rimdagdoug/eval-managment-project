686403703a693ad7e372c487f86767ce86f5994e
rim <rimdagdoug99@gmail.com>
2024-08-17 23:08:06 +0200

    add guards

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import HomePage from '@/pages/Auth/HomePage.vue';
 import LoginPage from '@/pages/Auth/LoginPage.vue';
 import EvalPages from '@/pages/eval/EvalPages.vue';
 import SkillsPage from '@/pages/skills/SkillsPage.vue';
+import { useAuthStore } from '@/stores/auth';
 import { createRouter, createWebHistory } from 'vue-router';
 
 
@@ -17,5 +18,17 @@ const router = createRouter({
     history: createWebHistory(),
     routes,
 })
+
+router.beforeEach((to, from, next) => {
+    const authStore = useAuthStore();
+    
+    const authRequired = ['/home', '/skills', '/eval'];
+
+    if (authRequired.includes(to.path) && !authStore.isAuthenticated) {
+        next('/login');
+    } else {
+        next();
+    }
+});
 
 export default router;
