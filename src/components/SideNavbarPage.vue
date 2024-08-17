ef645f2a7138a50a403d004793a74c4465d8fe2c
rim <rimdagdoug99@gmail.com>
2024-08-17 16:58:32 +0200

    add routes

--- src/components/SideNavbarPage.vue
+++ src/components/SideNavbarPage.vue
@@ -3,9 +3,9 @@
         <nav>
           <div class="side_navbar">
             <span>Main Menu</span>
-            <a href="#" class="active">Dashboard</a>
-            <a href="#">Skills</a>
-            <a href="#">Evaluation</a>
+            <router-link to="/home" class="active">Dashboard</router-link>
+            <router-link to="/skills">Skills</router-link>
+            <router-link to="/eval">Evaluation</router-link>
             <a href="#">User</a>
             <a href="#">Logout</a>
           </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import HomePage from '@/pages/Auth/HomePage.vue';
 import LoginPage from '@/pages/Auth/LoginPage.vue';
+import EvalPages from '@/pages/eval/EvalPages.vue';
+import SkillsPage from '@/pages/skills/SkillsPage.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 
@@ -7,6 +9,8 @@ const routes = [
     { path: '/', redirect: '/login' },
     { path: '/home', component: HomePage},
     { path: '/login', component: LoginPage },
+    {path: '/skills',component: SkillsPage},
+    {path: '/eval',component: EvalPages}
 ];
 
 const router = createRouter({
